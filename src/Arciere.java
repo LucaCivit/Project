d644fb56a557f1218bb7855e681807e4dae2aafb
andre4rge <andrea2argentieri@gmail.com>
2022-12-01 18:03:48 +0100

    Third Commit

--- src/Arciere.java
+++ src/Arciere.java
@@ -1,6 +1,6 @@
 public class Arciere extends Personaggio{
     public Arciere(){
         this.costo=20;
-        this.attacco=10900;
+        this.attacco=1000;
     }
 }
---
+++ src/GUI.java
@@ -0,0 +1,61 @@
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUI extends JFrame implements ActionListener{
+    private JButton Fanteb;
+    private JButton Spadaccinob;
+    private JButton Cavaliereb;
+    private JButton Arciereb;
+    private JButton Magob;
+    private JButton Catapultab;
+    private JButton Cannoneb;
+    private JButton Incendiariab;
+    private JButton Giganteb;
+    private JButton Dragob;
+    private JTextField Guadagno;
+    public GUI(){
+        super("Battaglia");
+        Guadagno=new JTextField("0");
+        Fanteb=new JButton("Fante");
+        Spadaccinob=new JButton("Spadaccino");
+        Cavaliereb=new JButton("Cavaliere");
+        Arciereb=new JButton("Arciere");
+        Magob=new JButton("Mago");
+        Catapultab=new JButton("Catapulta");
+        Cannoneb=new JButton("Cannone");
+        Incendiariab=new JButton("Incendiaria");
+        Giganteb=new JButton("Gigante");
+        Dragob=new JButton("Drago");
+
+        Fanteb.addActionListener(this);
+        Spadaccinob.addActionListener(this);
+        Cavaliereb.addActionListener(this);
+        Arciereb.addActionListener(this);
+        Magob.addActionListener(this);
+        Catapultab.addActionListener(this);
+        Cannoneb.addActionListener(this);
+        Incendiariab.addActionListener(this);
+        Giganteb.addActionListener(this);
+        Dragob.addActionListener(this);
+
+        JPanel p1=new JPanel(new GridLayout(5,2));
+        p1.add(Fanteb); p1.add(Catapultab);
+        p1.add(Spadaccinob); p1.add(Cannoneb);
+        p1.add(Cavaliereb); p1.add(Incendiariab);
+        p1.add(Arciereb); p1.add(Giganteb);
+        p1.add(Magob); p1.add(Dragob);
+        JPanel p2=new JPanel(new BorderLayout());
+        p2.add(Guadagno,BorderLayout.NORTH); p2.add(p1,BorderLayout.SOUTH);
+
+        setContentPane(p2);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(600,600);
+        setVisible(true);
+
+    }
+
+
+}
