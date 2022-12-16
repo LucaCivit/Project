462a511eb5dd37cb16ff1d43bb901a242ea28c72
andre4rge <andrea2argentieri@gmail.com>
2022-12-16 19:53:50 +0100

    interfaccia3

--- Progetto/core/src/com/project/clicker/Fante.java
+++ Progetto/core/src/com/project/clicker/Fante.java
@@ -1,9 +1,15 @@
 package com.project.clicker;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+
 public class Fante extends Personaggio {
-    public Fante(){
+
+    public Fante(String text, Skin skin) {
+        super(text, skin);
         this.costo=0;
-        this.attacco=1;
+        this.attacco=50;
     }
 }
 
--- Progetto/core/src/com/project/clicker/Personaggio.java
+++ Progetto/core/src/com/project/clicker/Personaggio.java
@@ -1,9 +1,35 @@
 package com.project.clicker;
 
-public abstract class Personaggio {
-    int costo;
-    int attacco;
-    public Personaggio(){};
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.EventListener;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+
+import java.awt.*;
+import java.awt.event.InputEvent;
+
+public  abstract class Personaggio extends TextButton {
+   int costo;
+   int attacco;
+   Label l;
+   Boolean b=false;
+    public Personaggio(String text, Skin skin,Label label) {
+        super(text, skin);
+        this.l=label;
+        addListener(new ClickListener(){
+            public void clicked(InputEvent e, float x, float y){
+                if(Integer.parseInt(l.getText())>=costo){
+                    setTouchable();
+                }
+                else{
+                    l.setText(Integer.toString(Integer.parseInt(l.getText())+attacco));
+                }
+            }
+        });
+    }
 
     public int getCosto() {
         return costo;
@@ -12,4 +38,5 @@ public abstract class Personaggio {
     public int getAttacco() {
         return attacco;
     }
+
 }
